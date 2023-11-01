a0a33442535c813929afe04cd68bb0539bc257f8
Alexander Silich <sili41995@gmail.com>
2023-11-01 15:32:52 +0200

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,10 @@
 import About from '@/components/About';
 
+export const metadata = {
+  title: 'About',
+  description: 'Page about project Phonebook',
+};
+
 const AboutPage = () => {
   return <About />;
 };
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,11 @@
 import AuthForm from '@/components/AuthForm';
 import LoginForm from '@/components/LoginForm';
 
+export const metadata = {
+  title: 'Login',
+  description: 'Page for login user',
+};
+
 const LoginPage = () => (
   <AuthForm>
     <LoginForm />
