cb1189c84836ba523bbb6bdd393215b593cef2d4
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-12-17 20:42:04 +0200

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,8 +8,8 @@ const About: FC = () => (
     employers. A server application was developed to securely store information.
     This application was developed without the Axios to improve skills in
     working with HTTP requests. Because under the hood of the Redux Toolkit is
-    Immer - we can freely "mutate" the state, and Immer will do it right to keep
-    us updated.
+    Immer - we can freely &quot;mutate&quot; the state, and Immer will do it
+    right to keep us updated.
   </Message>
 );
 
